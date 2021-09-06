f1faa39929ed47fb6a28f5a02099cce7c67ee2a0
Josh Mann <65541818+jlmdev@users.noreply.github.com>
2021-09-06 17:04:24 -0400

    Added remaining tasks

--- NightWatch/ContentView.swift
+++ NightWatch/ContentView.swift
@@ -12,52 +12,69 @@ struct ContentView: View {
         VStack {
             HStack {
                 VStack(alignment: .leading) {
-                    HStack {
-                        Text(
-                        Image(systemName: "moon.stars"))
-                            .foregroundColor(/*@START_MENU_TOKEN@*/.yellow/*@END_MENU_TOKEN@*/)
-                            .font(.title3)
-                            .fontWeight(.heavy)
-                        Text("Nightly Tasks")
-                            .underline()
-                            .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
-                            .fontWeight(.heavy)
-                            .foregroundColor(/*@START_MENU_TOKEN@*/.yellow/*@END_MENU_TOKEN@*/)
-                            .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+                    // MARK: Nightly Tasks
+                    Group {
+                        HStack {
+                            Text(
+                            Image(systemName: "moon.stars"))
+                                .foregroundColor(/*@START_MENU_TOKEN@*/.yellow/*@END_MENU_TOKEN@*/)
+                                .font(.title3)
+                                .fontWeight(.heavy)
+                            Text("Nightly Tasks")
+                                .underline()
+                                .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
+                                .fontWeight(.heavy)
+                                .foregroundColor(/*@START_MENU_TOKEN@*/.yellow/*@END_MENU_TOKEN@*/)
+                                .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+                        }
+                        Text("Check all windows")
+                        Text("Check all doors")
+                        Text("Check that the safe is locked")
+                        Text("Check the Mailbox")
+                        Text("Inspect security cameras")
+                        Text("Clear ice from sidewalks")
+                        Text("Document \"strange and unusual\" occurrences")
                     }
-                    Text("Check all windows")
-                    Text("Check all doors")
-                    Text("Check that the safe is locked")
-                    Text("Check the Mailbox")
-                    Text("Inspect security cameras")
-                    Text("Clear ice from sidewalks")
-                    Text("Document \"strange and unusual\" occurrences")
-                    HStack {
-                        Text(Image(systemName: "sunset"))
-                            .foregroundColor(/*@START_MENU_TOKEN@*/.yellow/*@END_MENU_TOKEN@*/)
-                            .font(.title3)
-                            .fontWeight(.heavy)
-                        Text("Weekly Tasks")
-                            .underline()
-                            .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
-                            .fontWeight(.heavy)
-                            .foregroundColor(/*@START_MENU_TOKEN@*/.yellow/*@END_MENU_TOKEN@*/)
-                            .padding(.top)
-                            .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+                    
+                    // MARK: Weekly Tasks
+                    Group {
+                        HStack {
+                            Text(Image(systemName: "sunset"))
+                                .foregroundColor(/*@START_MENU_TOKEN@*/.yellow/*@END_MENU_TOKEN@*/)
+                                .font(.title3)
+                                .fontWeight(.heavy)
+                            Text("Weekly Tasks")
+                                .underline()
+                                .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
+                                .fontWeight(.heavy)
+                                .foregroundColor(/*@START_MENU_TOKEN@*/.yellow/*@END_MENU_TOKEN@*/)
+                                .padding(.top)
+                                .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+                        }
+                        Text("Check inside all vacant rooms")
+                        Text("Walk the perimeter of the property")
                     }
-                    HStack {
-                        Text(Image(systemName: "calendar"))
-                            .foregroundColor(/*@START_MENU_TOKEN@*/.yellow/*@END_MENU_TOKEN@*/)
-                            .font(.title3)
-                            .fontWeight(.heavy)
-                        Text("Monthly Tasks")
-                            .underline()
-                            .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
-                            .fontWeight(.heavy)
-                            .foregroundColor(/*@START_MENU_TOKEN@*/.yellow/*@END_MENU_TOKEN@*/)
-                            .padding(.top)
-                            .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+                    
+                    // MARK: Monthly Tasks
+                    Group {
+                        HStack {
+                            Text(Image(systemName: "calendar"))
+                                .foregroundColor(/*@START_MENU_TOKEN@*/.yellow/*@END_MENU_TOKEN@*/)
+                                .font(.title3)
+                                .fontWeight(.heavy)
+                            Text("Monthly Tasks")
+                                .underline()
+                                .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
+                                .fontWeight(.heavy)
+                                .foregroundColor(/*@START_MENU_TOKEN@*/.yellow/*@END_MENU_TOKEN@*/)
+                                .padding(.top)
+                                .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+                        }
+                        Text("Test security alarm")
+                        Text("Test motion detectors")
+                        Text("Test smoke alarms")
                     }
+                    
                 }
                 .foregroundColor(.gray)
                 Spacer()
